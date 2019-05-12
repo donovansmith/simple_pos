33c2f2a0ef4dd5c4c43b930b9750a6fa3e0c9569
m2vang <m.vang02@yahoo.com>
2019-05-12 16:11:02 -0500

    Trying again

--- src/simple_pos/Login.java
+++ src/simple_pos/Login.java
@@ -1,9 +1,37 @@
 package simple_pos;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Login {
 	private String userName;
 	private String password;
 	private String name;
+	
+	BufferedReader br = null;
+	String line = "";
+	
+	Login() {
+		try {
+			br = new BufferedReader(new FileReader("Login.csv"));
+			while((line = br.readLine()) != null) {
+				String item[] = line.split(",");
+			}
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(br != null) {
+				try {
+					br.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 	
 	public void authenticate(String userName, String password) {
 		this.userName = userName;
